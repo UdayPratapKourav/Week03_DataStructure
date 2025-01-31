ff4d1b35073fdf5f53fd9e4f39d7e7192e9e614e
UdayPratapKourav <udaypratpkourav19@gmail.com>
2025-01-31 23:43:26 +0530

    commit change

--- Day02_StackQueueHashMap/src/hashmapshashfunctions/checkforapairwithgivensuminanarray/Main.java
+++
@@ -1,9 +0,0 @@
-package hashmapshashfunctions.checkforapairwithgivensuminanarray;
-
-public class Main {
-    public static void main(String[] args) {
-        int [] arr={2,7,11,15};
-        int largestSum=9;
-        PairWithTargetSum.hasPairWithSum(arr,largestSum);
-    }
-}
--- Day02_StackQueueHashMap/src/hashmapshashfunctions/findallsubarrayswithzerosum/Main.java
+++
@@ -1,8 +0,0 @@
-package hashmapshashfunctions.findallsubarrayswithzerosum;
-
-public class Main {
-    public static void main(String[] args) {
-        int [] arr={3, 4, -7, 1, 3, 3, 1, -4, -2, -2};
-        ZeroSumSubarrays.findZeroSumSubarrays(arr);
-    }
-}
